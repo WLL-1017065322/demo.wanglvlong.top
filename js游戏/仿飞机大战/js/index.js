e604f9c6ceb19af0f6862b3ec6ed5fa35418d1f4
WLL-1017065322 <1017065322@qq.com>
2019-05-31 23:56:13 +0800

    111

--- js游戏/仿飞机大战/js/index.js
+++ js游戏/仿飞机大战/js/index.js
@@ -19,3 +19,33 @@ jiandan.onclick = function(){
     gamestart.style.opacity=0;
 
 }
+
+
+function minePlane(pos){
+var mineImg = new Image();
+
+mineImg.width = 70;
+mineImg.height = 70;
+mineImg.style.position = "absolute"
+
+mineImg.src = "images/plane_0.png";
+mineImg.className = "minePlane";
+
+var left = pos.x - mineImg.width/2;
+var top = pos.y - mineImg.height/2;
+
+
+mineImg.style.left = left +"px";
+mineImg.style.top = top +"px";
+
+// 添加到地图
+map.appendChild(mineImg);
+
+// 飞机跟随鼠标动
+document.
+
+
+
+}
+
+minePlane({x:500,y:768});
